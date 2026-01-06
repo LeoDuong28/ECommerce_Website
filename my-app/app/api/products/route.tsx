a8bb631c3912124ca97d2b63846e95cf8e855b92
LeoDuong28 <duongtrongnghia287@gmail.com>
2026-01-06 01:16:09 -0800

    new

--- my-app/app/api/products/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { dbConnect } from "@/lib/db";
-import Product from "@/models/Product";
-
-export async function GET() {
-  await dbConnect();
-  const products = await Product.find().sort({ createdAt: -1 });
-  return NextResponse.json(products);
-}
